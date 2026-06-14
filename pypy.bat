134c31f1b8e86cf35d7fa3ee93006a8cb52634a7
AnixWindy <196703104+anixwindy@users.noreply.github.com>
2026-06-14 22:36:13 +0800

    Add files via upload

---
+++ pypy.bat
@@ -0,0 +1,4 @@
+@echo off
+rem 開啟 Python 專案視窗 (Pythis)
+rem code 是 .cmd，必須用 call 呼叫
+call code "D:\Cthis\Pythis.code-workspace"
---
+++ rtrt.bat
@@ -0,0 +1,4 @@
+@echo off
+rem 開啟 Rust 專案視窗 (RRust)
+rem code 是 .cmd，必須用 call 呼叫
+call code "D:\Cthis\RRust.code-workspace"
